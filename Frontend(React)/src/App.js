ad1cfc1093e9822b902c29086138d419e18c0a67
plorente005 <paulorente005@gmail.com>
2025-01-09 11:26:18 +0100

    Incluir el back

---
+++ Frontend(React)/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Login from './Pages/LoginPage/LoginPage';
+import ControlPanel from './Pages/ControlPanelPage/ControlPanelPage';
+import Station from './Pages/StationPage/StationPage';
+import HomePage from './Pages/HomePage/HomePage';
+import Configuration from './Pages/ConfigurationPage/ConfigurationPage';
+import PrivateRoute from './components/routes/PrivateRoute';
+import { AuthProvider } from './context/AuthContext';
+
+import './App.css';
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            {/* Página Principal */}
+            <Route path="/" element={<HomePage />} />
+            {/* Página de inicio de sesión */}
+            <Route path="/login" element={<Login />} />
+            {/* Ruta protegida para el Panel de Control */}
+            <Route path="/panel-control" element={<PrivateRoute element={<ControlPanel/>} />} />
+            {/* Página de configuración de la estación */}
+            <Route path="/configurar-estacion" element={<Configuration/>} />
+            {/* Página de detalles de la estación meteorológica */}
+            <Route path="/estacion" element={<Station/>} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
